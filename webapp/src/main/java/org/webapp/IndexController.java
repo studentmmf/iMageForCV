e8671184b5dc437793a02101eb19aafcf2f7be34
Dmitry Savkin <dmitry.savkin.info@gmail.com>
2019-08-28 23:48:25 +0200

    Creating spring mvc application

---
+++ webapp/src/main/java/org/webapp/IndexController.java
@@ -0,0 +1,17 @@
+package org.webapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("")
+public class IndexController  {
+
+
+    @RequestMapping(method =  RequestMethod.GET)
+    public static  String indexPage(ModelMap map) {
+        return "index";
+    }
+}
